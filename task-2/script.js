a0b27f4a362855705578c363aacb563f2d92bfcd
66redBastard <66redbastard@gmail.com>
2021-07-17 22:40:34 +0300

    commets added, const naming WIP

--- task-2/script.js
+++ task-2/script.js
@@ -1,72 +1,64 @@
-const initialTextData = "цей текс редагуємий. вміщує поля для вводу {{input}}, а такоє поля які вміщують якусь інформацію {{input:кокойто длинний текст}},содержт область {{textarea}} без тексту та  содержить область {{textarea:кокойто текст которий может мати двікрапки}} з іфнформацією"
+const initialTextData =
+  "цей текс редагуємий. вміщує поля для вводу {{input}}, а такоє поля які вміщують якусь інформацію {{input:кокойто великий текст}}, має область {{textarea}} без тексту та область {{textarea:кокойто текст, який может мати :двікрапки}} з іфнформацією";
 
 const btnImport = $("#renderResult");
-const textAreaImport = $("#mainTextArea")
+const textAreaImport = $("#mainTextArea");
 const showResult = $("#showText");
 const btnExport = $("#exportData");
 
-
 function importText(text) {
-    const regExInput = /{{input:[^}}]*}}/g;
-    const regExTextArea = /{{textarea:[^}}]*}}/g;
-   
-    return text
-        .replaceAll("{{input}}", "<input type='text'>")
-        .replaceAll("{{textarea}}", "<textarea></textarea>")
-        .replaceAll(regExInput, (value) => {
-            const cutStart = "{{input:";
-            const startIndex = cutStart.length;
-            const endIndex = value.length - 2;
-            const cuttedString = value.substring(startIndex, endIndex);
-            return `<input type='text' value="${cuttedString}">`;
-        })
-        .replaceAll(regExTextArea, (value) => {
-            const cutStart = "{{textarea:";
-            const startIndex = cutStart.length;
-            const endIndex = value.length - 2;
-            const cuttedString = value.substring(startIndex, endIndex);
-            return `<textarea>${cuttedString}</textarea>`;
-        });
+  const regExInput = /{{input:[^}}]*}}/g;
+  const regExTextArea = /{{textarea:[^}}]*}}/g;
+
+  return text
+    .replaceAll("{{input}}", "<input type='text'>")
+    .replaceAll("{{textarea}}", "<textarea></textarea>")
+    .replaceAll(regExInput, (value) => {
+      const cutStart = "{{input:"; // detect which part to be cutted
+      const startIndex = cutStart.length; // detect how many symbols to be cutted from the start of a string
+      const endIndex = value.length - 2; // detect how many symbols to be cutted from the end of a string
+      const cuttedString = value.substring(startIndex, endIndex); // .substring func that cuts input string
+      return `<input type='text' value="${cuttedString}">`;
+    })
+    .replaceAll(regExTextArea, (value) => {
+      const cutStart = "{{textarea:"; // same logic for the text area
+      const startIndex = cutStart.length;
+      const endIndex = value.length - 2;
+      const cuttedString = value.substring(startIndex, endIndex);
+      return `<textarea>${cuttedString}</textarea>`;
+    });
 }
 
 function exportText(html) {
-    console.log(html);
+  const str = $(html)
+    .clone() // create deep copy of the set of matched elements
+    .children() // find all of input and textarea as children el in the $(html)
+    .replaceWith(function () {
+      // this function is replacing string with help of this keyword
+      if (this instanceof HTMLInputElement) {
+        return `{{input:${this.value}}}`;
+      } else {
+        return `{{textarea: ${this.value}}}`;
+      }
+    })
+    .end()
+    .text();
 
-    // const str = [...html.children].map(x => {
-    //     if(x instanceof HTMLInputElement) {
-    //        return `{{input:${x.value}}}`;
-    //     } else {
-    //         return `{{textarea: ${x.value}}}`;
-    //     }
-    // });
-
-    const str = $(html).clone().children().replaceWith(function() {
-        console.log(this)
-        if(this instanceof HTMLInputElement) {
-                return `{{input:${this.value}}}`;
-            } else {
-                return `{{textarea: ${this.value}}}`;
-            }
-        console.log(arguments)
-    }).end().text();
-
-    console.log(str);
-
-    return str;
+  return str;
 }
 
 btnImport.click(() => {
-    const textToImport = textAreaImport.val();
-    const parsedText = importText(textToImport);
+  const textToImport = textAreaImport.val(); // get the value from textarea #mainTextArea
+  const parsedText = importText(textToImport); // fires import function with param
 
-    showResult.html(parsedText);
-})
+  showResult.html(parsedText); // show <p> tag with imported text and ability to change text from input and textarea fields
+});
 
 btnExport.click(() => {
-    const textToExport = showResult[0];
-    const exportedRwsult = exportText(textToExport);
-    textAreaImport.val(exportedRwsult);
-})
+  const textToExport = showResult[0];
+  console.log(textToExport);
+  const exportedRwsult = exportText(textToExport); // fires export text function with param to export back to the textarea #mainTextArea
+  textAreaImport.val(exportedRwsult); // updates text afted input and textarea is changed by the user
+});
 
-
-textAreaImport.val(initialTextData);
+textAreaImport.val(initialTextData); // show initial string in #mainTextArea
